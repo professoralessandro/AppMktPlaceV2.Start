45be5055d467c8484d76762efb00dccb28299a37
Alessandro dos Santos <professor_alessandro@hotmail.com>
2024-08-19 20:53:25 -0300

    shore: updated jwt package

--- AppMktPlaceV2.Start.Application/obj/Debug/net8.0/AppMktPlaceV2.Start.Application.AssemblyInfo.cs
+++ AppMktPlaceV2.Start.Application/obj/Debug/net8.0/AppMktPlaceV2.Start.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppMktPlaceV2.Start.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00ae46669e7cc513725fe15d77d50056b732e9ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a98a1499d997017dedb7346fb76023d3569c08")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppMktPlaceV2.Start.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppMktPlaceV2.Start.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
